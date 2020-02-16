e7a72c9bd667da2e8bcbb7be1c5916b514972058
Robert M Ricci <rmricci121@gmail.com>
2020-02-16 21:18:15 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -4,9 +4,9 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 3, 'male', 'brown', 'good', 0);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 3, 'male', 'brown', 'good', 0);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 3, 'male', 'brown', good, 0);
-INSERT INTO bears (name, age, gender, color, temperament, alice) VALUES ('Melissa', 3, 'male, brown, good, 0');
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 3, 'male, brown, good, 0');
 INSERT INTO bears (name, age, gender, color, temperament, alice) VALUES ('Grinch', 3, 'male, brown, good, 0');
 INSERT INTO bears (name, age, gender, color, temperament, alice) VALUES ('Wendy', 3, 'male, brown, good, 0');
-INSERT INTO bears (name, age, gender, color, temprament, alice) VALUES (null, null, null, brown, good, 0');
+INSERT INTO bears (name, age, gender, color, temperament, alice) VALUES (null, null, null, brown, good, 0');
 
 
