7bc9636627f903427c8cb181119d07f645a1702b
Robert M Ricci <rmricci121@gmail.com>
2020-02-16 21:07:37 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,4 +1,11 @@
 def insert_bears
-  
+  INSERT INTO bears (name, age, gender, color, temprament, alice) VALUES ('Mr. Chocolate', 3, 'male, brown, good, 0');
+  INSERT INTO cats (name, age, breed) VALUES ('Maru', 3, 'Scottish Fold');
+  INSERT INTO cats (name, age, breed) VALUES ('Maru', 3, 'Scottish Fold');
+  INSERT INTO cats (name, age, breed) VALUES ('Maru', 3, 'Scottish Fold');
+  INSERT INTO cats (name, age, breed) VALUES ('Maru', 3, 'Scottish Fold');
+  INSERT INTO cats (name, age, breed) VALUES ('Maru', 3, 'Scottish Fold');
+  INSERT INTO cats (name, age, breed) VALUES ('Maru', 3, 'Scottish Fold');
+  INSERT INTO cats (name, age, breed) VALUES ('Maru', 3, 'Scottish Fold');
 
 end
