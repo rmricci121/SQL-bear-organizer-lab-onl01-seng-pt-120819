3a97c128cc006ec9beadd035934715376718d3c0
Robert M Ricci <rmricci121@gmail.com>
2020-02-16 21:14:39 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,5 +1,5 @@
 
-  INSERT INTO bears (name, age, gender, color, temperament, alice) VALUES ('Mr. Chocolate', 3, 'male, brown, good, 0');
+  INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 3, 'male, brown, good, 0');
 "INSERT INTO bears (name, age, gender, color, temprament, alice) VALUES ('Rowdy', 3, 'male, brown, good, 0');"
 "INSERT INTO bears (name, age, gender, color, temprament, alice) VALUES ('Tabitha', 3, 'male, brown, good, 0');"
 "INSERT INTO bears (name, age, gender, color, temprament, alice) VALUES ('Sergeant Brown', 3, 'male, brown, good, 0');"
