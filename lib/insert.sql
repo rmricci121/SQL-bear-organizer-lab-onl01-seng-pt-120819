d4f0c74d5cbe01732eca0930ec44a3a72ed95af7
Robert M Ricci <rmricci121@gmail.com>
2020-02-16 21:09:56 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,11 +1,11 @@
 def insert_bears
   INSERT INTO bears (name, age, gender, color, temprament, alice) VALUES ('Mr. Chocolate', 3, 'male, brown, good, 0');
-  INSERT INTO cats (name, age, breed) VALUES ('Maru', 3, 'Scottish Fold');
-  INSERT INTO cats (name, age, breed) VALUES ('Maru', 3, 'Scottish Fold');
-  INSERT INTO cats (name, age, breed) VALUES ('Maru', 3, 'Scottish Fold');
-  INSERT INTO cats (name, age, breed) VALUES ('Maru', 3, 'Scottish Fold');
-  INSERT INTO cats (name, age, breed) VALUES ('Maru', 3, 'Scottish Fold');
-  INSERT INTO cats (name, age, breed) VALUES ('Maru', 3, 'Scottish Fold');
-  INSERT INTO cats (name, age, breed) VALUES ('Maru', 3, 'Scottish Fold');
+INSERT INTO bears (name, age, gender, color, temprament, alice) VALUES ('Rowdy', 3, 'male, brown, good, 0');
+INSERT INTO bears (name, age, gender, color, temprament, alice) VALUES ('Tabitha', 3, 'male, brown, good, 0');
+INSERT INTO bears (name, age, gender, color, temprament, alice) VALUES ('Sergeant Brown', 3, 'male, brown, good, 0');
+INSERT INTO bears (name, age, gender, color, temprament, alice) VALUES ('Melissa', 3, 'male, brown, good, 0');
+INSERT INTO bears (name, age, gender, color, temprament, alice) VALUES ('Grinch', 3, 'male, brown, good, 0');
+INSERT INTO bears (name, age, gender, color, temprament, alice) VALUES ('Wendy', 3, 'male, brown, good, 0');
+INSERT INTO bears (name, age, gender, color, temprament, alice) VALUES (null, null, null, brown, good, 0');
 
 end
