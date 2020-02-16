35f6ce75432fab272f27798dda62f1b6dd8699e3
Robert M Ricci <rmricci121@gmail.com>
2020-02-16 21:10:55 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -4,8 +4,8 @@ def insert_bears
 "INSERT INTO bears (name, age, gender, color, temprament, alice) VALUES ('Tabitha', 3, 'male, brown, good, 0');"
 "INSERT INTO bears (name, age, gender, color, temprament, alice) VALUES ('Sergeant Brown', 3, 'male, brown, good, 0');"
 "INSERT INTO bears (name, age, gender, color, temprament, alice) VALUES ('Melissa', 3, 'male, brown, good, 0');"
-INSERT INTO bears (name, age, gender, color, temprament, alice) VALUES ('Grinch', 3, 'male, brown, good, 0');
-INSERT INTO bears (name, age, gender, color, temprament, alice) VALUES ('Wendy', 3, 'male, brown, good, 0');
-INSERT INTO bears (name, age, gender, color, temprament, alice) VALUES (null, null, null, brown, good, 0');
+"INSERT INTO bears (name, age, gender, color, temprament, alice) VALUES ('Grinch', 3, 'male, brown, good, 0');"
+"INSERT INTO bears (name, age, gender, color, temprament, alice) VALUES ('Wendy', 3, 'male, brown, good, 0');"
+"INSERT INTO bears (name, age, gender, color, temprament, alice) VALUES (null, null, null, brown, good, 0');"
 
 end
